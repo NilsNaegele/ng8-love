9142226a99a8ea3371fa17d415890f2069d059e3
NilsNaegele <nils.holger.naegele@gmail.com>
2019-08-25 23:02:45 +0200

    quick fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
    firebase: {
-    apiKey: 'AIzaSyA1h4sv1G3nUF4jeO9wJX5QNuFMsjCC95U',
+    apiKey: 'AIzaSyA1h4sv1G3nUF4jeO9wJX5QNuFMsjCC95U1',
     authDomain: 'ng8-love.firebaseapp.com',
     databaseURL: 'https://ng8-love.firebaseio.com',
     projectId: 'ng8-love',
     storageBucket: 'ng8-love.appspot.com',
     messagingSenderId: '845276427158',
-    appId: '1:845276427158:web:8b9c37bf0668b5f4'
+    appId: '1:845276427158:web:8b9c37bf0668b5f42'
   }
 };
 
